ee05ffcb7cf6964b5c22923901fb22d6702e2137
victor-rios03 <victor_rios03@protonmail.com>
2022-10-06 17:27:35 -0700

    nuevo metodo agregado

--- src/main/java/com/robles/tareaguitarra/TareaGuitarra.java
+++ src/main/java/com/robles/tareaguitarra/TareaGuitarra.java
@@ -12,6 +12,7 @@ package com.robles.tareaguitarra;
 public class TareaGuitarra {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GuitarString test = new GuitarString(10000);
+	test.pluck();
     }
 }
