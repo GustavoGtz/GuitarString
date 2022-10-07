30580bf485d854e7b09b0500034877ea2a1f042a
victor-rios03 <victor_rios03@protonmail.com>
2022-10-06 18:22:34 -0700

    Agregados meteodos fundamentales

--- src/main/java/com/robles/tareaguitarra/GuitarString.java
+++ src/main/java/com/robles/tareaguitarra/GuitarString.java
@@ -10,42 +10,42 @@ import java.util.Random;
  */
 public class GuitarString {
 
-	Queue<Double> circularBuffer = new LinkedList<>();
+    Queue<Double> circularBuffer = new LinkedList<>();
+    public static float energyDecay = 0.996f;
 
-	public GuitarString(double frequency) {
-		if (frequency <= 0) {
-			throw new IllegalArgumentException("Frecuencia no válida");
-		}
-		int n = (int) Math.round(StdAudio.SAMPLE_RATE / frequency);
-		if (n < 2) {
-			throw new IllegalArgumentException("Error de buffer circular");
-		}
-		for (int i = 0; i < n; i++) {
-			circularBuffer.add(0.0);
-		}
-	}
+    public GuitarString(double frequency) {
+        if (frequency <= 0) {
+            throw new IllegalArgumentException("Frecuencia no válida");
+        }
+        int n = (int) Math.round(StdAudio.SAMPLE_RATE / frequency);
+        if (n < 2) {
+            throw new IllegalArgumentException("Error de buffer circular");
+        }
+        for (int i = 0; i < n; i++) {
+            circularBuffer.add(0.0);
+        }
+    }
 
-	public GuitarString(double[] init) {
-		if (init.length < 2) {
-			throw new IllegalArgumentException("Error de buffer circular");
-		}
-		for (double item : init) {
-			circularBuffer.add(item);
-		}
-	}
+    public GuitarString(double[] init) {
+        if (init.length < 2) {
+            throw new IllegalArgumentException("Error de buffer circular");
+        }
+        for (double item : init) {
+            circularBuffer.add(item);
+        }
+        System.out.println(circularBuffer);
+    }
 
-	public void pluck() {
-		double randomValue;
-
-		for (int i = 0; i < circularBuffer.size(); i++) {
-			randomValue = Math.random() * (0.49999 + 0.5) - 0.5; //TODO: MEJORAR FORMULA
-			circularBuffer.remove();
-			circularBuffer.add(randomValue);
-		}
-	}
-	
-	public double sample() {
-		return circularBuffer.peek();
-	}
-
+    public void pluck() {
+        Random random = new Random();
+        
+    }
+    
+    public void tic(){
+        double value1 = circularBuffer.peek();
+        circularBuffer.remove();
+        double value2 = circularBuffer.peek();
+        double result = ((value1+value2)/2) * energyDecay;
+        circularBuffer.add(result);
+    }
 }
