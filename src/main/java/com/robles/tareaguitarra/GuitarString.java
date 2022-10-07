81043468b948f7dc001b8ae2bf150114be17e041
Gustabu <91583023+GustavoGtz@users.noreply.github.com>
2022-10-06 21:30:23 -0700

    A vr qpdo
    Leanlo, hize unos cambios y puse algunos comentairos donde falta agregar cosas.
    En los metodos, el unico que si acaso necesita comentarios  es el de tic, los otros dos se explican por si solos.

--- src/main/java/com/robles/tareaguitarra/GuitarString.java
+++ src/main/java/com/robles/tareaguitarra/GuitarString.java
@@ -1,51 +1,65 @@
-package com.robles.tareaguitarra;
+package com.robles.tareaguitarra; // ¿Esto lo dejamos?
 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
 /**
+ * Esta clase implementa . . . (Ponemos una explicacion a grandes
+ * rasgos de lo que hace el programa y cuales son las condiciones
+ * para que funcione.
  *
- * @autores Santiago Robles Victor Rios Gustavo Gutierrez
+ * @autores Gabriel Santiago Robles Robles.
+ *          Victor Hugo Ramirez Rios.
+ *          Gustavo Gutierrez Navarro.
  */
+
 public class GuitarString {
 
-    Queue<Double> circularBuffer = new LinkedList<>();
-    public static float energyDecay = 0.996f;
-
-    public GuitarString(double frequency) {
-        if (frequency <= 0) {
-            throw new IllegalArgumentException("Frecuencia no válida");
+    Queue<Double> circularBuffer = new LinkedList<>(); // ¿La ponemos privada?
+    public final double DECAYCONSTANT = 0.996f;
+    
+    // ¿Ponemos una descrpcion del constructor? 
+    public GuitarString(double frequency){
+        if(frequency <= 0){
+            throw new IllegalArgumentException("El valor de la frecuencia es invalido, por favor ingrese un numero mayor a 0.");
         }
-        int n = (int) Math.round(StdAudio.SAMPLE_RATE / frequency);
-        if (n < 2) {
-            throw new IllegalArgumentException("Error de buffer circular");
+        int bufferSize = (int) Math.round(StdAudio.SAMPLE_RATE / frequency);
+        if(bufferSize < 2){
+            throw new IllegalArgumentException("El valor de la frecuencia es muy grande, por favor intente con un numero mas pequeño.");
         }
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < bufferSize; i++){
             circularBuffer.add(0.0);
         }
     }
-
-    public GuitarString(double[] init) {
-        if (init.length < 2) {
-            throw new IllegalArgumentException("Error de buffer circular");
+    
+    // ¿Ponemos una descrpcion del constructor? 
+    public GuitarString(double[] init){
+        if(init.length < 2){
+            throw new IllegalArgumentException("El numero de componentes es muy pequeño, por favor asegurese de mandar por lo menos 2 elementos.");
         }
-        for (double item : init) {
-            circularBuffer.add(item);
+        for(double value : init){ // value representa el desplazamiento de una cuerda con valor entre -0.5 y 0.5.
+            circularBuffer.add(value);
         }
-        System.out.println(circularBuffer);
     }
-
+    
     public void pluck() {
-        Random random = new Random();
-        
+        double randomValue;
+		for(int i = 0; i < circularBuffer.size(); i++){
+			randomValue = Math.random() * (1.4999999) - 0.5; // Valor aleatorio entre [-0.5, 0.5) 
+			circularBuffer.remove();
+			circularBuffer.add(randomValue);
+		} 
     }
     
     public void tic(){
-        double value1 = circularBuffer.peek();
+        double firstValue = circularBuffer.peek();
         circularBuffer.remove();
-        double value2 = circularBuffer.peek();
-        double result = ((value1+value2)/2) * energyDecay;
-        circularBuffer.add(result);
+        double secondValue = circularBuffer.peek();
+        circularBuffer.add((firstValue + secondValue)/2 * DECAYCONSTANT);
+    }
+    
+    publi double sample(){
+        return circularBuffer.peek();
     }
 }
