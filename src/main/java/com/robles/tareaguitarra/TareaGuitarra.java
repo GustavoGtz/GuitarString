d8adf02131ddbae62fb597b0e2d56e386fbd13de
victor-rios03 <victor_rios03@protonmail.com>
2022-10-06 17:42:00 -0700

    agregado metodo sample

--- src/main/java/com/robles/tareaguitarra/TareaGuitarra.java
+++ src/main/java/com/robles/tareaguitarra/TareaGuitarra.java
@@ -14,5 +14,6 @@ public class TareaGuitarra {
     public static void main(String[] args) {
         GuitarString test = new GuitarString(10000);
 	test.pluck();
+	    System.out.println("sample: " + test.sample());
     }
 }
